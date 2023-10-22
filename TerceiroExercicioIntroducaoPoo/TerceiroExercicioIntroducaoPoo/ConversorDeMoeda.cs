b9799353f3feb832f7afc621702c4bc2631206d2
Gabriel Lana <81386246+GabrielLana@users.noreply.github.com>
2023-10-22 20:54:17 -0300

    Add files via upload

---
+++ TerceiroExercicioIntroducaoPoo/TerceiroExercicioIntroducaoPoo/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerceiroExercicioIntroducaoPoo
+{
+    public static class ConversorDeMoeda
+    {
+        public static double ConverterReaisParaDolar(double cotacao, double qtd)
+        {
+            var bruto = cotacao * qtd;
+            var iof = (bruto / 100) * 6;
+            return bruto + iof;
+        }
+    }
+}
---
+++ TerceiroExercicioIntroducaoPoo/TerceiroExercicioIntroducaoPoo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using TerceiroExercicioIntroducaoPoo;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotacao, qtd;
+
+            Console.Write("Qual é a cotação do dólar? "); cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? "); qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine($"Valor a ser pago em reais = {ConversorDeMoeda.ConverterReaisParaDolar(cotacao, qtd).ToString("F2")}");
+        }
+    }
+}
